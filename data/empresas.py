3a1d6f972c954e32856f5c6a91581dd28eb788fc
Vicente H <vicente.herrera@iplusd.cl>
2026-04-07 13:59:42 -0400

    Configuración CI/CD con todas las empresas

--- data/empresas.py
+++ data/empresas.py
@@ -14,5 +14,93 @@ EMPRESAS = [
         "filtro_cargo":False,
         "Cargo": None,
 	"file_name": "BIOMETRIA"
-    }
+    },
+    {
+        "rut": "77128770-0",
+        "nombre": "CYGNUS (Alvarez Recursos Humanos Limitada)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "CYGNUS"
+    },
+    {
+        "rut": "92604000-6",
+        "nombre": "Empresa Nacional del Petroleo",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "EMPRESA_PETROLEO"
+    },
+    {
+        "rut": "87756500-9",
+        "nombre": "ENAP REFINERIAS S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_REFINERIAS"
+    },
+    {
+        "rut": "96579730-0",
+        "nombre": "Enap Sipetrol S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_SIPETROL"
+    },
+    {
+        "rut": "77091268-7",
+        "nombre": "NUEVA BIOMETRIA SPA",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "NUEVA_BIOMETRIA"
+    },
+    {
+        "rut": "91915000-9",
+        "nombre": "Sigdo Koppers (INGENIERIA Y CONSTRUCCION SIGDO KOPPERS S A)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SIGDO_KOOPERS"
+    },
+    {
+        "rut": "99598300-1",
+        "nombre": "SIGDO KOPPERS S.A. (SOCIEDADES DE INVERSION Y RENTISTAS DE CAPITALES MOBILIARIOS EN GENERAL)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SIGDO_KOOPERS_SA"
+    },
+    {
+        "rut": "78167971-2",
+        "nombre": "SIO RRHH (Outsourcing Integrales de Servicios S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SIO_RRHH"
+    },
+    {
+        "rut": "76788120-7",
+        "nombre": "SK CAPACITACION (SK CAPACITACION S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SK_CAPACITACION"
+    },
+    {
+        "rut": "76662490-1",
+        "nombre": "SK INDUSTRIAL (SK INDUSTRIAL S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SK_INDUSTRIAL"
+    },
+    {
+        "rut": "96717980-9",
+        "nombre": "SKCOMSA (CONSTRUCCIONES Y MONTAJES COM S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SKCOMSA"
+    },
 ]
