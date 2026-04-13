dfcb05d05298f29358f6dfe3b12ce10ff5593520
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-13 14:43:33 -0400

    Implementación Prueba-Email 6 - time exception- todos- si

--- data/empresas.py
+++ data/empresas.py
@@ -1,5 +1,68 @@
 EMPRESAS = [
     {
+        "rut": "79777010-8",
+        "nombre": "ALTERNATTIVA (MARKETING Y PROMOCIONES S.A)",
+        "reportes": ["jor_diaria", "diario"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "ALTERNATTIVA",
+	"url_menu": "https://alternattivadt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "76257834-4",
+        "nombre": "Biometria Aplicada Spa",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "BIOMETRIA",
+	"url_menu": "https://badt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "77128770-0",
+        "nombre": "CYGNUS (Alvarez Recursos Humanos Limitada)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "CYGNUS",
+	"url_menu": "https://cygnusdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "92604000-6",
+        "nombre": "Empresa Nacional del Petroleo",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "EMPRESA_PETROLEO",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "87756500-9",
+        "nombre": "ENAP REFINERIAS S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_REFINERIAS",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "96579730-0",
+        "nombre": "Enap Sipetrol S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_SIPETROL",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "77091268-7",
+        "nombre": "NUEVA BIOMETRIA SPA",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "NUEVA_BIOMETRIA",
+	"url_menu": "https://badt.baplicada.cl/Default.aspx"
+    },
+    {
         "rut": "91915000-9",
         "nombre": "Sigdo Koppers (INGENIERIA Y CONSTRUCCION SIGDO KOPPERS S A)",
         "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
@@ -16,5 +79,41 @@ EMPRESAS = [
         "Cargo": None,
 	"file_name": "SIGDO_KOOPERS_SA",
 	"url_menu": "https://skicdt.baplicada.cl/Default.aspx"
-    }
+    },
+    {
+        "rut": "78167971-2",
+        "nombre": "SIO RRHH (Outsourcing Integrales de Servicios S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SIO_RRHH",
+	"url_menu": "https://siodt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "76788120-7",
+        "nombre": "SK CAPACITACION (SK CAPACITACION S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SK_CAPACITACION",
+	"url_menu": "https://skicdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "76662490-1",
+        "nombre": "SK INDUSTRIAL (SK INDUSTRIAL S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SK_INDUSTRIAL",
+	"url_menu": "https://skicdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "96717980-9",
+        "nombre": "SKCOMSA (CONSTRUCCIONES Y MONTAJES COM S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SKCOMSA",
+	"url_menu": "https://skicdt.baplicada.cl/Default.aspx"
+    },
 ]
