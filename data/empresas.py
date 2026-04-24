1c8d64a17947e6a9ce55ac5563bddf327e9cf3f3
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-24 11:44:53 -0400

    test: Resumen general

--- data/empresas.py
+++ data/empresas.py
@@ -1,30 +1,48 @@
 EMPRESAS = [
     {
-        "rut": "79777010-8",
-        "nombre": "ALTERNATTIVA (MARKETING Y PROMOCIONES S.A)",
-        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
-        "filtro_cargo":True,
-        "Cargo": "Mercaderista Planta",
-	"file_name": "ALTERNATTIVA",
-	"nom_informe": "MARKETING Y PROMOCIONES S.A."
-    },
-    {
-        "rut": "76257834-4",
-        "nombre": "Biometria Aplicada Spa",
+        "rut": "92604000-6",
+        "nombre": "Empresa Nacional del Petroleo",
         "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
         "filtro_cargo":False,
         "Cargo": None,
-	"file_name": "BIOMETRIA",
-	"nom_informe": "Biometria Aplicada Spa"
+	"file_name": "EMPRESA_PETROLEO",
+	"nom_informe": "Empresa Nacional del Petroleo"
     },
     {
-        "rut": "77128770-0",
-        "nombre": "CYGNUS (Alvarez Recursos Humanos Limitada)",
+        "rut": "87756500-9",
+        "nombre": "ENAP REFINERIAS S.A",
         "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
-        "filtro_cargo":True,
-        "Cargo": "Mercaderista Planta",
-	"file_name": "CYGNUS",
-	"nom_informe": "Cygnus PDV"
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_REFINERIAS",
+	"nom_informe": "ENAP Refinerías"
+    },
+    {
+        "rut": "96579730-0",
+        "nombre": "Enap Sipetrol S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_SIPETROL",
+	"nom_informe": "Enap Sipetrol S.A."
+    },
+    {
+        "rut": "77091268-7",
+        "nombre": "NUEVA BIOMETRIA SPA",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "NUEVA_BIOMETRIA",
+	"nom_informe": "NUEVA BIOMETRIA SPA"
+    },
+    {
+        "rut": "91915000-9",
+        "nombre": "Sigdo Koppers (INGENIERIA Y CONSTRUCCION SIGDO KOPPERS S A)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SIGDO_KOOPERS",
+	"nom_informe": "INGENIERIA Y CONSTRUCCION SIGDO KOPPERS S A"
     }
 ]
 
